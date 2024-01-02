1d5582130eb75d204b73659b311ea00875aad3cf
Ryanhuii <yhryanchan@gmail.com>
2024-01-02 09:30:08 +0800

    testing GitHub 123

--- src/main/java/com/ryanhuii/tuitionfinder/SpringTuitionFinderApplication.java
+++ src/main/java/com/ryanhuii/tuitionfinder/SpringTuitionFinderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringTuitionFinderApplication {
-
+	// testing github 123
 	public static void main(String[] args) {
 		// don't use this line.
 		// Instead, use the javaFX Application class to run the application
