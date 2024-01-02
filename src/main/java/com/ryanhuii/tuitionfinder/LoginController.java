c23b8966aa787e1f8ab4234edeb4d8ca9c675931
Ryanhuii <yhryanchan@gmail.com>
2024-01-02 23:23:44 +0800

    Basic additions to the LoginController. Also hello from my macbook!

--- src/main/java/com/ryanhuii/tuitionfinder/LoginController.java
+++ src/main/java/com/ryanhuii/tuitionfinder/LoginController.java
@@ -1,4 +1,27 @@
 package com.ryanhuii.tuitionfinder;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import org.springframework.stereotype.Component;
 
+@Component
 public class LoginController {
+    @FXML
+    private Button btn_login;
+
+    @FXML
+    private TextField txt_password;
+
+    @FXML
+    private TextField txt_username;
+
+    @FXML
+    void onLogin(ActionEvent event) {
+        String username = txt_username.getText();
+        String password = txt_password.getText();
+        System.out.println(
+                "Username: " + username +
+                " Password: " + password);
+    }
 }
